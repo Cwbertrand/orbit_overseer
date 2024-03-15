528b2b8b2bf9174e0e04383fc4889c55a7e10f6d
Cwbertrand <cwbertrand8@gmail.com>
2024-03-15 23:58:50 +0100

    finished unique id generator

---
+++ src/components/CopyIdToClipboard/CopyIdToClipboard.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, TouchableOpacity, Text } from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+
+interface Props {
+    placeholder?: string;
+    style?: object;
+    viewStyles?: object;
+    disabled?: boolean;
+    text?: string; 
+}
+
+export const CopyIdToClipboard = ({ placeholder, style, viewStyles, disabled, text }: Props) => {
+    const [textValue, setTextValue] = useState<string>(text || '');
+    const [isCopied, setIsCopied] = useState(false);
+
+    const copyToClipboard = () => {
+        Clipboard.setString(textValue);
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000);
+    };
+
+    return (
+        <>
+            <TouchableOpacity
+                style={viewStyles}
+                onPress={copyToClipboard} 
+                disabled={isCopied}
+            >
+                <Text style={{color: '#fff'}}>{text}</Text>
+            </TouchableOpacity>
+            {isCopied && <Text style={styles.copiedText}>Copied</Text>}
+        </>
+        
+    );
+};
+
+const styles = StyleSheet.create({
+    copiedText: {
+        color: '#fff',
+        backgroundColor: '#16db65',
+        padding: 5,
+        position: 'absolute',
+        alignSelf: 'flex-end',
+        width: 60,
+    },
+});
