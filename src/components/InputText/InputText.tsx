f73988b862acbf4b9ca614177b2721ad58a95edc
Ugo DA SILVA <ugo.dasilva@ati4group.com>
2024-04-12 09:49:51 +0200

    harmonize folder's name for inputText component

--- src/components/InputText/InputText.tsx
+++ src/components/InputText/InputText.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { View, TextInput } from 'react-native';
 
 interface Props {
     placeholder?: string;
@@ -31,20 +31,3 @@ const InputText = ({placeholder, style, viewStyles, onSave}: Props) => {
 };
 
 export default InputText;
-
-const styles = StyleSheet.create({
-    // inputBox: {
-    //     display: "flex",
-    //     paddingVertical: 20,
-    //     paddingHorizontal: 20,
-    //     borderWidth: 2,
-    //     borderColor: 'rgba(255,255,255,0.15)',
-    //     flexDirection: "row",
-    //     marginBottom: 15,
-    // },
-    // textInput: {
-    //     width: '90%',
-    //     fontSize: 16,
-    //     color: '#fff',
-    // },
-});
