9584f53551a3aba0f7af17e4ba7b7de601e5d7cb
Cwbertrand <cwbertrand8@gmail.com>
2024-05-14 13:17:17 +0200

    create the game proper

--- src/screens/game/Game.screen.tsx
+++ src/screens/game/Game.screen.tsx
@@ -5,31 +5,67 @@ import {useNavigation} from "@react-navigation/native";
 import GameID from "../../components/GameID/GameID";
 import { useAppSelector } from "../../app/redux/store/store";
 import WebSocketService from "../../app/redux/slice/WebSocketService";
+import { Operation } from "../../models/operation";
+import { Element } from "../../models/element";
 
 const GameScreen = () => {
     const navigation = useNavigation();
     const { gameId, players, playerId, playerName } = useAppSelector(state => state.game);
-    const [operation, setOperation] = useState(null);
+    const [gameElement, setGameElement] = useState<Element[]>([]);
+    const [operation, setOperation] = useState<Operation>();
+    const [integrity, setIntegrity] = useState<number>(100);
     // Get the singleton instance of the WebSocketService
     const webSocketService = WebSocketService.getInstance(); 
 
     useEffect(() => {
+        handleGame();
+    }, []); 
+    
+    const handleGame = () => {
         if(gameId) {
             if(webSocketService.socket){
                 webSocketService.socket.onmessage = (e: MessageEvent) => {
                     console.log(e.data);
+                    const dataValue = e.data.data;
                     if (e.type === 'operation') {
-                        setOperation(e.data);
+                        try {
+                            if (dataValue.role === 'operator') {
+                                console.log(dataValue);
+                                setGameElement(dataValue.element);
+                                dataValue.description
+                                setTimeout(() => {
+                                    setGameElement([]);
+                                    dataValue.description = "En attente de la prochaine opération...";
+                                }, dataValue.duration * 1000);
+                            }
+                            if (dataValue.role === 'instructor') {
+                                console.log(dataValue);
+                                setGameElement(dataValue.element);
+                                dataValue.description
+                                setTimeout(() => {
+                                    setGameElement([]);
+                                    dataValue.description = "En attente de la prochaine opération...";
+                                }, dataValue.duration * 1000);
+                            }
+                            setOperation(dataValue);
+                        } catch (error) {
+                            throw error;
+                        }
+
+                    }
+                    if (e.type === "integrity") {
+                        try {
+                            setIntegrity(dataValue.integrity);
+                        } catch (err) {
+                            console.error("Error parsing message:", err);
+                        }
                     }
                     
                 }
             }
         }
-    }, []); 
 
-    
-
-    
+    }
 
     return (
         <>
